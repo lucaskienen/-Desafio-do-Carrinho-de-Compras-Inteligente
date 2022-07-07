074700c940b8e4f7741e9a8eea0974bf3bd0c254
Lucas <rota_kienen@hotmail.com>
2022-07-07 10:22:03 -0300

    Adicao de plural nas notas

--- Desafio_Carrinho.py
+++ Desafio_Carrinho.py
@@ -32,43 +32,77 @@ def finalizar():
         total = total + produtos[nome]
         print(nome)
     print(f"Valor total : \n {total}")
+
     if total >= 100:
         contador = 0
         while total >= 100:
             contador = contador + 1
             total = total - 100
-        print(f"{contador} nota de 100")
+        if contador == 1:
+            print(f"{contador} nota de 100")
+        else:
+            print(f"{contador} notas de 100")
+
+    if total >= 50:
+        contador = 0
+        while total >= 50:
+            contador = contador + 1
+            total = total - 50
+        if contador == 1:
+            print(f"{contador} nota de 50")
+        else:
+            print(f"{contador} notas de 50")
+
     if total >= 20:
         contador = 0
         while total >= 20:
             contador = contador + 1
             total = total - 20
-        print(f"{contador} nota de 20")
+        if contador == 1:
+            print(f"{contador} nota de 20")
+        else:
+            print(f"{contador} notas de 20")
+
     if total >= 10:
         contador = 0
         while total >= 10:
             contador = contador + 1
             total = total - 10
-        print(f"{contador} nota de 10")
+        if contador == 1:
+            print(f"{contador} nota de 10")
+        else:
+            print(f"{contador} notas de 10")
 
     if total >= 5:
         contador = 0
         while total >= 5:
             contador = contador + 1
             total = total - 5
-        print(f"{contador} nota de 5")
+        if contador == 1:
+            print(f"{contador} nota de 5")
+        else:
+            print(f"{contador} notas de 5")
+
     if total >= 2:
         contador = 0
         while total >= 2:
             contador = contador + 1
             total = total - 2
-        print(f"{contador} nota de 2")
+        if contador == 1:
+            print(f"{contador} nota de 2")
+        else:
+            print(f"{contador} notas de 2")
+
     if total >= 1:
         contador = 0
         while total >= 1:
             contador = contador + 1
             total = total - 1
-        print(f"{contador} nota de 1")
+            if contador == 1:
+                print(f"{contador} nota de 1")
+            else:
+                print(f"{contador} notas de 1")
+
     exit()
 
 
